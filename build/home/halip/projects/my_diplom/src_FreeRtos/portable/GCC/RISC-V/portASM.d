883004e02490027b591a631201edca6b19656dec
ntitan <halip0503@gmail.com>
2023-05-01 15:33:47 +0300

    clean

--- build/home/halip/projects/my_diplom/src_FreeRtos/portable/GCC/RISC-V/portASM.d
+++
@@ -1,6 +0,0 @@
-build//home/halip/projects/my_diplom/src_FreeRtos/portable/GCC/RISC-V/portASM.o: \
- /home/halip/projects/my_diplom/src_FreeRtos/portable/GCC/RISC-V/portASM.S \
- /home/halip/projects/my_diplom/src_FreeRtos/portable/GCC/RISC-V/portContext.h \
- /home/halip/projects/my_diplom/src_FreeRtos/portable/GCC/RISC-V/chip_specific_extensions/RV32I_CLINT_no_extensions/freertos_risc_v_chip_specific_extensions.h
-/home/halip/projects/my_diplom/src_FreeRtos/portable/GCC/RISC-V/portContext.h:
-/home/halip/projects/my_diplom/src_FreeRtos/portable/GCC/RISC-V/chip_specific_extensions/RV32I_CLINT_no_extensions/freertos_risc_v_chip_specific_extensions.h:
